dfd9ec8b21e6567ec63c2a6e32c058d7eed877af
Marcos Vallecillos Usagre <marcosvallecillosu@gmail.com>
2024-02-05 20:17:12 +0100

    Extrayendo titulares

--- index2.sh
+++ index2.sh
@@ -8,3 +8,7 @@ if ! command -v wget &> /dev/null; then
     else
     echo "El comando wget esta instalado"
 fi
+# Descargar el código fuente HTML del portal de noticias
+wget -O marca.html $noticias
+# Extraer titulares con grep y awk (ajusta según la estructura del HTML)
+grep -o '<h2 class="titulo">.*</h2>' noticias.html | awk -F"[<>]" '{print $3}' > titulares.txt
